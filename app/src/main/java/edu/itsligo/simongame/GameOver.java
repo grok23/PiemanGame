7c16c81d4637535eed19ef37b01b6f1df147ecc8
grok23 <s00189752@mail.itsligo.ie>
2020-12-23 14:47:50 +0000

    making git repo again

---
+++ app/src/main/java/edu/itsligo/simongame/GameOver.java
@@ -0,0 +1,58 @@
+package edu.itsligo.simongame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class GameOver extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    EditText playerName;
+    TextView score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        sharedPreferences = getSharedPreferences("score_ref", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        int s = sharedPreferences.getInt("score",0);
+        playerName = findViewById(R.id.playerName);
+
+        score = findViewById(R.id.score);
+        score.setText(String.valueOf(s));
+
+
+
+
+        editor.putInt("score",0);
+        editor.commit();
+    }
+
+    public void doReplay(View view) {
+
+        Intent i = new Intent(GameOver.this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void doHighScore(View view) {
+
+
+        editor.putString("playerName", playerName.getText().toString());
+        int s = sharedPreferences.getInt("score", 0);
+        editor.commit();
+
+        Intent i = new Intent(GameOver.this, HighScores.class);
+        i.putExtra("score",s);
+
+        startActivity(i);
+    }
+}
